f480e5569e0f604fa88e6b78f693294c7a66fca9
Manuel Guiu <manuel.guiu@baboonlab.com>
2026-01-19 15:20:22 +0100

    refactor: remove unnecessary import statement in calc.py and clean up whitespace in api.py

--- app/api.py
+++ app/api.py
@@ -9,11 +9,9 @@ CALCULATOR = Calculator()
 api_application = Flask(__name__)
 HEADERS = {"Content-Type": "text/plain", "Access-Control-Allow-Origin": "*"}
 
-
 @api_application.route("/")
 def hello():
     return "Hello from The Calculator!\n"
-
 
 @api_application.route("/calc/add/<op_1>/<op_2>", methods=["GET"])
 def add(op_1, op_2):
@@ -22,7 +20,6 @@ def add(op_1, op_2):
         return ("{}".format(CALCULATOR.add(num_1, num_2)), http.client.OK, HEADERS)
     except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
-
 
 @api_application.route("/calc/substract/<op_1>/<op_2>", methods=["GET"])
 def substract(op_1, op_2):
